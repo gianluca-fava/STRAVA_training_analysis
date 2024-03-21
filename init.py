89334fecaa805f6ad4a31cbc1ecce0fdb0f28f97
gianluca-fava <80771631+gianluca-fava@users.noreply.github.com>
2024-03-21 11:37:39 +0100

    map correct

--- init.py
+++ init.py
@@ -2,6 +2,8 @@ import requests
 import os
 from dotenv import load_dotenv
 import json
+from datetime import datetime
+from random import randrange
 
 ## READ .env
 
@@ -35,13 +37,10 @@ try:
 
     # Setting parameters for the request to retrieve activities data
     param = {'per_page': 200, 'page': 1}
-    data = requests.get(activites_url, headers=header, params=param, verify=True).json()
 
-    file_path = "data.json"
-    with open(file_path, 'w') as file:
-        json.dump(data, file)
+    new_data = requests.get(activites_url, headers=header, params=param, verify=True).json()
 
-    print("Activities data has been successfully retrieved and saved to", file_path)
+    print("Activities data has been successfully retrieved")
 
 except requests.exceptions.RequestException as e:
     print("Error occurred during request:", e)
@@ -51,3 +50,28 @@ except KeyError as e:
 
 except Exception as e:
     print("An unexpected error occurred:", e)
+
+with open('data.json', 'r') as file:
+    existing_data = json.load(file)
+    if existing_data:
+        last_activity_date = datetime.strptime(existing_data[0]['start_date'][:-1],
+                                               "%Y-%m-%dT%H:%M:%S")  # Remove the timezone part (Z) from the string and parse it
+
+# check if the 2 date are different otherwise it's a mess
+if datetime.strptime(new_data[0]['start_date'][:-1], "%Y-%m-%dT%H:%M:%S") != last_activity_date:
+    # Append new activities to additional_data until we find one with start date before the last activity in existing data
+    additional_data = []  # only the data following the last date in data
+    i = 0
+    while datetime.strptime(new_data[i]['start_date'][:-1], "%Y-%m-%dT%H:%M:%S") > last_activity_date:
+        additional_data.append(new_data[i])
+        i += 1
+
+    # Add random color to all the activities (for the plot in the map)
+    for activity in additional_data:
+        activity["color"] = "#{:02x}{:02x}{:02x}".format(randrange(255), randrange(255), randrange(255))
+
+    data_extended = additional_data + existing_data
+
+    # Overwrite json
+    with open("data.json", "w") as file:
+        json.dump(data_extended, file, indent=4)
